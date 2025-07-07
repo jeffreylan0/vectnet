9b923ddfb9ad6af348daba465918d0f96a1493d7
jeffreylan0 <127343526+jeffreylan0@users.noreply.github.com>
2025-07-06 17:03:17 -0700

    Delete src directory

--- src/routes/recognize.js
+++
@@ -1,7 +0,0 @@
-import { recognizeShape } from '../controllers/shapeController.js';
-
-async function routes(fastify, options) {
-  fastify.post('/recognize', recognizeShape);
-}
-
-export default routes;
